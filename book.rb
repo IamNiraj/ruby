5d97d806c4376f6b55908d3fef9425b6be117329
Niraj Pradhan <redragons94@gmail.com>
2014-07-16 14:38:30 +0545

    Added first ruby file

---
+++ book.rb
@@ -0,0 +1,82 @@
+@endall ="y"
+@library=[] #array of book hash
+
+while @endall=="y" do
+puts "1.Enter the book record"
+puts "2.Enter the name of book to search"
+puts "3.Enter the author to search"
+puts "4.List the book and its auhtor"
+puts "Enter y to Exit"
+puts "Enter your choice"
+choice = gets.chomp
+case choice
+when '1'
+	endwhile ="y"
+	book={}
+	def book(btitle,author) #hash of book title and author
+	  {
+	    btitle:btitle,
+	    author:author
+	  }	  
+	end
+	
+	while endwhile=="y" do  #Choice to Enter the book 
+	    btitle = ""
+	    puts "Title of the book"
+	    btitle = gets.chomp
+	    puts "Enter the author of #{btitle}"
+	    author = gets.chomp
+	    book(btitle,author)
+	    @library.push(book(btitle,author))    
+	    puts "DO you want to enter another book (Press y to continue)"
+	    endwhile =gets.chomp
+	end
+	puts "The book is admitted to Library"
+	
+when '2'
+	def find_by_title (title) 
+	  if @library.find { |book| "#{book[:btitle]}" == title }
+	   puts  " found" 
+	  else
+	    puts "Book not Found"
+	  end
+	end
+	endsearch= "n"
+	while endsearch == "n"  do
+	puts "Enter the name of the book to search"
+	@searchtitle=gets.chomp
+	puts find_by_title(@searchtitle)
+	puts "Do you want to continue your search (Press y to exit)"
+	endsearch = gets.chomp
+end
+when '3'
+	def find_by_author (author) 
+	  if @library.find {   |book| "#{book[:author]}" == author  }
+	  		puts "Books available"
+	  	# @library.each do |book|
+  		# puts  "#{book[:btitle]} is written by #{book[:author]} \n" if "#{book[:author]}" == author " 
+ 		# end
+	  else
+	    puts "The library has no book by #{author} "
+	  end
+	end
+	endsearch= "n"
+	while endsearch == "n"  do
+	puts "Enter the name of author to search"
+	@searchtitle=gets.chomp
+	puts find_by_author(@searchtitle)
+	puts "Do you want to continue your search (Press y to exit)"
+	endsearch = gets.chomp
+end
+when '4'
+	puts "The library has following"
+	@library.each do |book|
+  		puts  "#{book[:btitle]} is written by #{book[:author]} \n " 
+ 	end
+when 'y'
+	@endall = "n"
+else 
+	puts "Invalid Choice"
+end
+
+end
